8283161cb53f86895e081012a46c550e0e20fa36
jacky.shen <jacky.shen@paradm.com>
2020-04-23 16:31:44 +0800

    init

---
+++ SmartShare/sse-spring-boot/sse-web/src/main/java/com/paradm/sse/WebApplication.java
@@ -0,0 +1,17 @@
+package com.paradm.sse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Jacky.shen
+ * @create data 2020/4/23
+ */
+@SpringBootApplication
+public class WebApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(WebApplication.class, args);
+  }
+
+}
