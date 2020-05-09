4be8e29ee2f485cff41b19f1d7b1aac99d28fbef
Jacky.shen <jacky318520@126.com>
2020-05-09 13:18:40 +0800

    init

--- SmartShare/sse-spring-boot/sse-framework/src/main/java/com/paradm/sse/framework/configuration/I18nConfiguration.java
+++ SmartShare/sse-spring-boot/sse-framework/src/main/java/com/paradm/sse/framework/configuration/I18nConfiguration.java
@@ -1,4 +1,4 @@
-package com.paradm.sse.configuration;
+package com.paradm.sse.framework.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
